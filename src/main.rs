6f1d38fec500c9367aa6c04dedfeddb5294106cb
dineshkandhi <123708112+dineshkandhi@users.noreply.github.com>
2023-12-13 18:00:02 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,13 @@ use crate::draw::to_coord_u32;
 
 const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
 
-
-
 fn main() {
     let (width, height) = (30, 30);
+
+    // Get player name from the user
+    let mut player_name = String::new();
+    println!("Enter your name:");
+    std::io::stdin().read_line(&mut player_name).expect("Failed to read line");
 
     let mut window: PistonWindow =
         WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
@@ -24,7 +27,7 @@ fn main() {
             .build()
             .unwrap();
 
-    let mut game = Game::new(width, height);
+    let mut game = Game::new(width, height, player_name.trim().to_string());
     while let Some(event) = window.next() {
         if let Some(Button::Keyboard(key)) = event.press_args() {
             game.key_pressed(key);
@@ -36,6 +39,15 @@ fn main() {
 
         event.update(|arg| {
             game.update(arg.dt);
+
+           
         });
+         
+        
     }
+    
+     // Check if the game is over and display the score in the console
+    
+        println!("Game Over! {} - Score: {}", game.get_player_name(), game.get_score());
+     
 }
